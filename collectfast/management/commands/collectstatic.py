8730e04b529362380e103865ef5297907058d864
Kevin Glavin <archen.sol@gmail.com>
2014-03-06 17:26:37 -0500

    test to see if handle_noargs needs to be explicitly declared

--- collectfast/management/commands/collectstatic.py
+++ collectfast/management/commands/collectstatic.py
@@ -2,22 +2,12 @@
 
 from __future__ import with_statement
 
-# Python 2/3 support for command line input
-try:
-    import __builtin__
-    input = getattr(__builtin__, 'raw_input')
-except (ImportError, AttributeError):
-    pass
-
 import hashlib
 from optparse import make_option
 import datetime
 
 from django.contrib.staticfiles.management.commands import collectstatic
 from django.core.cache import cache
-from django.core.files.storage import FileSystemStorage
-from django.core.management.base import CommandError
-from django.utils.encoding import smart_str
 
 
 class Command(collectstatic.Command):
@@ -86,7 +76,10 @@ class Command(collectstatic.Command):
 
                 # Create md5 checksum from local file
                 file_contents = local_file.read()
-                local_etag = '"%s"' % hashlib.md5(file_contents).hexdigest()
+                try:
+                    local_etag = '"%s"' % hashlib.md5(file_contents).hexdigest()
+                except TypeError:
+                    local_etag = '"%s"' % hashlib.md5(file_contents.encode('utf-8')).hexdigest()
 
                 # Compare checksums and skip copying if matching
                 if storage_lookup.etag == local_etag:
@@ -114,58 +107,3 @@ class Command(collectstatic.Command):
             self.log(u"Deleting '%s'" % path)
             self.storage.delete(prefixed_path)
         return True
-
-    def handle_noargs(self, **options):
-        self.set_options(**options)
-        # Warn before doing anything more.
-        if (isinstance(self.storage, FileSystemStorage) and
-                self.storage.location):
-            destination_path = self.storage.location
-            destination_display = ':\n\n    %s' % destination_path
-        else:
-            destination_path = None
-            destination_display = '.'
-
-        if self.clear:
-            clear_display = 'This will DELETE EXISTING FILES!'
-        else:
-            clear_display = 'This will overwrite existing files!'
-
-        if self.interactive:
-            confirm = input(u"""
-You have requested to collect static files at the destination
-location as specified in your settings%s
-
-%s
-Are you sure you want to do this?
-
-Type 'yes' to continue, or 'no' to cancel: """
-% (destination_display, clear_display))
-            if confirm != 'yes':
-                raise CommandError("Collecting static files cancelled.")
-
-        collected = self.collect()
-        modified_count = len(collected['modified'])
-        unmodified_count = len(collected['unmodified'])
-        post_processed_count = len(collected['post_processed'])
-
-        if self.verbosity >= 1:
-            template = ("Collected static files in %(collect_time)s."
-                        "\nSkipped %(num_skipped)i already synced files."
-                        "\n%(modified_count)s %(identifier)s %(action)s"
-                        "%(destination)s%(unmodified)s%(post_processed)s.\n")
-            summary = template % {
-                'modified_count': modified_count,
-                'identifier': 'static file' + (modified_count != 1 and 's' or ''),
-                'action': self.symlink and 'symlinked' or 'copied',
-                'destination': (destination_path and " to '%s'"
-                                % destination_path or ''),
-                'unmodified': (collected['unmodified'] and ', %s unmodified'
-                               % unmodified_count or ''),
-                'post_processed': (collected['post_processed'] and
-                                   ', %s post-processed'
-                                   % post_processed_count or ''),
-                'num_skipped': self.num_skipped_files,
-                'collect_time': self.collect_time,
-            }
-            self.stdout.write(smart_str(summary))
